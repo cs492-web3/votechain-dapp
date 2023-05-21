ef9f79254f29c12a961d1192923c4049e0235747
seajung <chung1114@kaist.ac.kr>
2023-05-21 15:13:35 +0900

    Updates

---
+++ _next/static/4O-xOKX2lfKq_H1ybaOsP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d36b11106a1f3b8e.css","static/chunks/pages/index-405d2e3c50380f4a.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
